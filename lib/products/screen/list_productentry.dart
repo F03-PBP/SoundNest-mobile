db6c0b3a053bd04f717e94313f71c9907bdfb1f0
TheoKevH <theodorekevinh@gmail.com>
2024-12-22 08:45:21 +0700

    fix color error

--- lib/products/screen/list_productentry.dart
+++ lib/products/screen/list_productentry.dart
@@ -117,7 +117,7 @@ class _ProductEntryCardsState extends State<ProductEntryCards> {
                           borderRadius: BorderRadius.circular(16),
                           boxShadow: [
                             BoxShadow(
-                              color: Colors.black.withValues(alpha: 0.1),
+                              color: Colors.black.withOpacity(0.1),
                               blurRadius: 4,
                               spreadRadius: 1,
                             ),
